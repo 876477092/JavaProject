f7b1cd09cd12d5c86fd8a0dcb6e152bb2d9c4439
876477092 <876477092@qq.com>
2016-12-24 21:04:00 +0800

    User    First
    UserAppointment

---
+++ UserAppointment/text.java
@@ -0,0 +1,13 @@
+package hospital;
+
+public class text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1;
+		System.out.println(x);
+		UserAppointment.AddUserAppointment();
+
+	}
+
+}
